d32b4a67d3457d1a2f835e2ee5ea0719aa28da38
dscs4thakkar <dscs4thakkar@yahoo.com>
2019-09-22 14:16:24 -0500

    homework finished but still needs to update as some functions not working

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,73 @@
+var topics = ["Dog", "Dolphin", "Lion", "Tiger", "Elephant",
+"Monkey", "Panda", "Snake", "Cat", "Zibra",
+"Crocodile", "Fish", "Shark", "Hippo", "Bull",
+"Deer", "Duck", "Eagle"];
+var numberOfGIFs = 10;
+var cutOffRating = "PG";
+
+function renderButtons(){
+	for(var i = 0; i < topics.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("animal-button");
+		newButton.text(topics[i]);
+		$("#button-container").append(newButton);
+	}
+	$(".animal-button").unbind("click");
+
+	$(".animal-button").on("click", function(){
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		populateGIFContainer($(this).text());
+	});
+
+}
+
+function addButton(show){
+	if(topics.indexOf(show) === -1) {
+		topics.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
+
+function populateGIFContainer(show){
+	$.ajax({
+		url: "https://api.giphy.com/v1/gifs/search?q=' + animal + '&api_key=No6am1UpZmhO4GCEncGIfkDJNUE7aino&limit=10",
+		method: "GET"
+	}).then(function(response){
+		response.data.forEach(function(element){
+			newDiv = $("<div>");
+			newDiv.addClass("individual-gif-container");
+			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
+			newImage.addClass("gif-image");
+			newImage.attr("state", "still");
+			newImage.attr("still-data", element.images.fixed_height_still.url);
+			newImage.attr("animated-data", element.images.fixed_height.url);
+			newDiv.append(newImage);
+			$("#gif-container").append(newDiv);
+		});
+		
+		$(".gif-image").unbind("click");
+		$(".gif-image").on("click", function(){
+			if($(this).attr("state") === "still") {
+				$(this).attr("state", "animated");
+				$(this).attr("src", $(this).attr("animated-data"));
+			}
+			else {
+				$(this).attr("state", "still");
+				$(this).attr("src", $(this).attr("still-data"));
+			}
+		});
+	});
+}
+
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#animal-show").val().trim());
+		$("#animal-show").val("");
+	});
+});
